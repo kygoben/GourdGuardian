84cb8ac6a0660fbf6ca8d68af442613b8c3231b4
Kyle Goben <kygoben@gmail.com>
2023-03-21 14:40:40 -0500

    added image to end screen

--- Frontend/src/pages/volunteer/end.js
+++ Frontend/src/pages/volunteer/end.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Image from 'next/image'
-
+import PumpkinData from '@/components/Pumpkin';
 import styles from '@/styles/end.module.css';
 
 function end() {
   return (
     <div className={styles.center}>
     <h1>Please make sure the stencil code is written on the back</h1>
-    <Image src={'/pumpkin.jpg'} alt="sucks" width='400' height='400' />
+    <PumpkinData className={styles.back} sid= {'11-34'} title= {'Mockingjay'} category= {'Hunger Games'}></PumpkinData>
+    <Image src={'/pumpkin2.jpg'} alt="sucks" width='400' height='400' />
     <div className={styles.buttons}>
     <button className={styles.button}>Enter new stencil ID</button>
     <button className={styles.buttonNo}>Take a picture with your pumpkin!</button>
