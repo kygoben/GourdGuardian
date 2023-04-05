c52a49465b1bb01039cd9be2e62e480dd973f203
Kyle Goben <kygoben@gmail.com>
2023-04-04 22:01:31 -0500

    post status/id api finished and working

--- Frontend/src/pages/api/status/[sid].js
+++ Frontend/src/pages/api/status/[sid].js
@@ -2,25 +2,49 @@ import excuteQuery from "/lib/db.js";
 
 export default async function handler(req, res) {
   const { sid } = req.query;
-  
-  const time = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  
+
+  const time = new Date().toISOString().slice(0, 19).replace("T", " ");
+
   if (req.method === "POST") {
-    const result = await excuteQuery({
-      query:
-        'UPDATE sstatus SET tracing_start="'+time+'" WHERE sid=?;',
+
+    
+    const s = await excuteQuery({
+      query: "SELECT * FROM sstatus where sid = ?;",
       values: [sid],
     });
-    console.log(result);
+
+    if (s[0].carving_start) {
+      const result = await excuteQuery({
+        query: 'UPDATE sstatus SET carving_end="' + time + '" WHERE sid=?;',
+        values: [sid],
+      });
+    } else if (s[0].tracing_end) {
+      const result = await excuteQuery({
+        query: 'UPDATE sstatus SET carving_start="' + time + '" WHERE sid=?;',
+        values: [sid],
+      });
+    } else if (s[0].tracing_start) {
+      const result = await excuteQuery({
+        query: 'UPDATE sstatus SET tracing_end="' + time + '" WHERE sid=?;',
+        values: [sid],
+      });
+    } else if (s[0].cutting) {
+      const result = await excuteQuery({
+        query: 'UPDATE sstatus SET tracing_start="' + time + '" WHERE sid=?;',
+        values: [sid],
+      });
+    }
+
+    res.status(200).json({ stage: "3", status: "2" });    // console.log(result);
   } else {
-  try {
-    const result = await excuteQuery({
-      query: "SELECT * FROM status where sid = ?;",
-      values: [sid],
-    });
-    // console.log(result);
-    res.status(200).json({"stage":"3", "status": "2"});
-  } catch (err) {
-    console.log(err);
-  }}
+    try {
+      const result = await excuteQuery({
+        query: "SELECT * FROM sstatus where sid = ?;",
+        values: [sid],
+      });
+      res.status(200).json({ stage: "3", status: "2" });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
