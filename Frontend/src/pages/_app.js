26482c87599c85f7c9b3d785f89d4a715fa541bc
Kyle Goben <kygoben@gmail.com>
2023-08-23 09:34:26 -0500

    bug fix

--- Frontend/src/pages/_app.js
+++ Frontend/src/pages/_app.js
@@ -2,7 +2,10 @@ import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <><Component {...pageProps} />
-          <Analytics />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
         </>
+    );
 }
