c66f22b549768516db93487fcc0623562bb975a9
Kyle Goben <kygoben@gmail.com>
2023-08-25 23:46:51 -0500

    Login styling

--- Frontend/src/pages/login.js
+++ Frontend/src/pages/login.js
@@ -42,26 +42,47 @@ export default function Login() {
 
   return (
     <>
-      <input
-        name="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <br></br>
-      <br></br>
-      <button onClick={handleSignUp}>Sign up</button>
-      <br></br>
-      <br></br>
-      <button onClick={handleSignIn}>Sign in</button>
-      <br></br>
-      <br></br>
-      <button onClick={handleSignOut}>Sign out</button>
+    <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl font-bold mb-4">Sign In</h1>
+          <div className="flex flex-col justify-center items-center">
+            <input
+              className="border-2 border-black rounded-md w-72 p-2 mb-2"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+            <input
+              className="border-2 border-black rounded-md w-72 p-2 mb-2"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex flex-row justify-center items-center">
+            <button
+              className="border-2 border-black rounded-md w-32 p-2 m-2"
+              onClick={handleSignIn}
+            >
+              Sign In
+            </button>
+            <button
+              className="border-2 border-black rounded-md w-32 p-2 m-2"
+              onClick={handleSignUp}
+            >
+              Sign Up
+            </button>
+            <button
+              className="border-2 border-black rounded-md w-32 p-2 m-2"
+              onClick={handleSignOut}
+            >
+              Sign Out
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
