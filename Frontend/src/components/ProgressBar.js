5fbf3102b3b816b7a45e8e269562adbeee0d0961
Kyle Goben <kygoben@gmail.com>
2023-09-05 13:11:52 -0500

    Table data update and confirmation banner

--- Frontend/src/components/ProgressBar.js
+++ Frontend/src/components/ProgressBar.js
@@ -6,7 +6,6 @@ const ProgressBar = () => {
     color: "#fff",
     padding: "10px",
     display: "flex",
-    justifyContent: "space-between",
     alignItems: "center",
   };
 
@@ -17,18 +16,38 @@ const ProgressBar = () => {
     color: "#fff",
   };
 
-  const linkStyle = {
-    textDecoration: "none",
+  const progressBarContainer = {
+    display: "flex",
+    alignItems: "center",
+  };
+
+  const progressBarStyle = {
+    width: "200px", // Adjust the width as needed
+    height: "20px",
+    backgroundColor: "#ccc",
+    borderRadius: "10px",
+    margin: "0 10px",
+  };
+
+  const progressBarFillStyle = {
+    width: "39%", // Set the initial progress value here
+    height: "100%",
+    backgroundColor: "#007bff", // Change the color of the progress bar fill
+    borderRadius: "10px",
+    textAlign: "center",
+    lineHeight: "20px",
     color: "#fff",
-    marginLeft: "10px",
   };
 
   return (
     <div style={navbarStyle}>
-      <div> ProgressBar</div>
-      <progress id="file" value="32" max="100">
-        39%
-      </progress>
+      {/* <div style={logoStyle}>Your App Name</div> */}
+      <div style={progressBarContainer}>
+        <div>Progress: </div>
+        <div style={progressBarStyle}>
+          <div style={progressBarFillStyle}>39%</div>
+        </div>
+      </div>
     </div>
   );
 };
--- Frontend/src/components/StatusData.js
+++ Frontend/src/components/StatusData.js
@@ -3,9 +3,39 @@ import { useRouter } from "next/router";
 import { supabase } from "./../../supabaseConnection.js";
 import { useState } from "react"; // Import useEffect and useState
 import compareAsc from "date-fns/compareAsc";
+import index from "@/pages/index.js";
 
 const StatusData = ({ year, week, stage }) => {
   const [data, setData] = useState([]); //db values
+  const [successMessage, setSuccessMessage] = useState(null);
+  
+
+  /* CSS in a separate .css file or within your component */
+  const successStyle = {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    background: "rgba(0, 128, 0, 0.7)" /* Use rgba to set transparency */,
+    color: "#fff",
+    padding: "10px",
+    alignItems: "center",
+    z: 9999 /* Ensure it appears on top of everything */,
+  };
+
+  const buttonStyle = {
+    backgroundColor: 'green',
+    width: '24px',
+    height: '24px',
+    border: 'none',
+    borderRadius: '4px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    color: 'white',
+    fontSize: '16px',
+  };
 
   const navbarStyle = {
     background: "#111",
@@ -34,50 +64,98 @@ const StatusData = ({ year, week, stage }) => {
   }, [year, week, stage]);
 
   const getData = async () => {
-    let { data, statusError } = await supabase
+    let { data: stencilsData, error: stencilsError } = await supabase
+      .from("stencils")
+      .select("sid, title");
+
+    console.log(stencilsData, stencilsError);
+
+    let { data: sstatusData, error: sstatusError } = await supabase
       .from("sstatus")
-      .select("*")
-      .eq("year", year)
-      .eq("week", week)
-      .is("tracing_confirmed", null);
+      .select(
+        "sid, year, week, tracing_start, tracing_end, tracing_confirmed, tracer"
+      );
 
-    console.log(data, statusError);
+    console.log(sstatusData, sstatusError);
 
-    if (!data) {
+    const combinedData = stencilsData
+      .map((stencil) => {
+        const relatedSStatus = sstatusData.find((s) => s.sid === stencil.sid);
+        if (
+          relatedSStatus &&
+          relatedSStatus.year === year &&
+          relatedSStatus.week === week &&
+          !relatedSStatus.tracing_confirmed
+        ) {
+          return {
+            ...stencil,
+            sstatus: relatedSStatus,
+          };
+        }
+        return null; // Exclude entries with no matching "sstatus" or "year" not equal to '2024'
+      })
+      .filter(Boolean);
+
+    console.log(combinedData);
+
+    if (!combinedData) {
+      console.log("no data");
       return;
     }
 
-    data.sort((a, b) => {
-      const [aSid] = a.sid.split("-").map(Number);
-      const [bSid] = b.sid.split("-").map(Number);
-      return compareAsc(aSid, bSid);
+    combinedData.sort((a, b) => {
+      const [aSid, aSuffix] = a.sid.split("-").map(Number);
+      const [bSid, bSuffix] = b.sid.split("-").map(Number);
+
+      // Compare the main "sid" values numerically
+      if (aSid !== bSid) {
+        return aSid - bSid;
+      }
+
+      // If the main "sid" values are equal, compare the suffixes numerically
+      return aSuffix - bSuffix;
     });
-    console.log(data, statusError);
-    setData(data);
+    // console.log(data, statusError);
+    setData(combinedData);
   };
 
   const handleEdit = async (item, field, value) => {
-    //query
     console.log(item, field, value);
-
-
+    // Update the database
     const updateObject = { [field]: value };
+    let { data, error } = await supabase
+      .from("sstatus")
+      .update(updateObject)
+      .eq("sid", item.sid)
+      .select();
 
-    await supabase
-    .from("sstatus")
-    .update(updateObject)
-    .eq("sid", item.sid)
-    .select();
+    console.log(data, error);
 
-    getData();
+    if (!error) {
+      setSuccessMessage("Entry updated successfully"); // Set success message
+      getData();
+    } else {
+      // Handle error if needed
+    }
+
+    // Optionally, you can clear the success message after a few seconds
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, 3000); // Clear the message after 5 seconds
+
+    // getData();
   };
 
   if (data.length === 0) {
     return <div>No data available</div>;
   }
 
+  const currentDate = new Date();
+
   return (
     <div>
+      {successMessage && <div style={successStyle}>{successMessage}</div>}
+
       <div style={navbarStyle}></div>
       <table style={{ borderCollapse: "collapse", width: "100%" }}>
         <thead>
@@ -86,42 +164,54 @@ const StatusData = ({ year, week, stage }) => {
             <th style={tableHeaderStyle}>title</th>
             <th style={tableHeaderStyle}>tracing_start</th>
             <th style={tableHeaderStyle}>tracing_end</th>
-            <th style={tableHeaderStyle}>tracing_confirmed</th>
+            {/* <th style={tableHeaderStyle}>tracing_confirmed</th> */}
             <th style={tableHeaderStyle}>tracer</th>
             <th style={tableHeaderStyle}>confirm?</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td style={tableCellStyle}>
-                <input
-                  type="text"
-                  value={item.sid}
-                  onChange={(e) => handleEdit(item, "sid", e.target.value)}
-                />
-              </td>
+          {data.map((item, rowIndex) => (
+            <tr key={item.sid}>
+              <td style={tableCellStyle}>{item.sid}</td>
               <td style={tableCellStyle}>{item.title}</td>
-              <td style={tableCellStyle}>{item.tracing_start}</td>
-              <td style={tableCellStyle}>{item.tracing_end}</td>
               <td style={tableCellStyle}>
                 <input
                   type="datetime-local" // Use datetime-local input type for date and time
-                  value={item.tracing_confirmed}
+                  value={item.sstatus.tracing_start || ""}
+                  onChange={(e) =>
+                    handleEdit(item, "tracing_start", e.target.value)
+                  }
+                ></input>
+              </td>
+              <td style={tableCellStyle}>
+                <input
+                  type="datetime-local" // Use datetime-local input type for date and time
+                  value={item.sstatus.tracing_end || ""}
+                  onChange={(e) =>
+                    handleEdit(item, "tracing_end", e.target.value)
+                  }
+                ></input>
+              </td>
+              {/* <td style={tableCellStyle}>
+                <input
+                  type="datetime-local" // Use datetime-local input type for date and time
+                  value={item.sstatus.tracing_confirmed || ""}
                   onChange={(e) =>
                     handleEdit(item, "tracing_confirmed", e.target.value)
                   }
                 />
-              </td>
+              </td> */}
               <td style={tableCellStyle}>
                 <input
                   type="text"
-                  value={item.tracer}
+                  value={item.sstatus.tracer || ""}
                   onChange={(e) => handleEdit(item, "tracer", e.target.value)}
                 />
               </td>
               <td style={tableCellStyle}>
-                <button>confirm</button>
+              <button style={buttonStyle} onClick={() => handleEdit(item, "tracing_confirmed", currentDate.toISOString())}>
+      ✓
+    </button>
               </td>
             </tr>
           ))}
