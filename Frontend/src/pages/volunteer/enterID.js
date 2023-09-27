11104247beb163afd450bf0e5b6c8acb678d5566
Kyle Goben <kygoben@gmail.com>
2023-09-26 20:41:26 -0500

    volunteer error message

--- Frontend/src/pages/volunteer/enterID.js
+++ Frontend/src/pages/volunteer/enterID.js
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import styles from "@/styles/enterID.module.css";
 import Link from "next/link";
 import { supabase } from "../../../supabaseConnection.js";
-import { parse } from 'cookie';
 import SignInPrompt from "@/components/VolunteerSignInPrompt.js";
 
 export default function Home() {
@@ -12,61 +11,56 @@ export default function Home() {
 
   const getStencil = async () => {
     const sid = document.getElementById("pid").value;
-    
-    console.log(document.cookie);
 
-    const cookieHeader = document.cookie;
-    const parsedCookies = parse(cookieHeader); // Parse cookies using the 'parse' method
-
-    console.log(parsedCookies.name); // Access the 'name' cookie
-
-    // console.log(cookies().get('name'));
-
-    let{data: admin_data, error: adminError} = await supabase
-            .from("admin_data")
-            .select('*');
-            console.log(admin_data[0]);
+    let { data: admin_data, error: adminError } = await supabase
+      .from("admin_data")
+      .select("*");
+    console.log(admin_data[0]);
     const year = admin_data[0].year;
     const week = admin_data[0].week;
     const stage = admin_data[0].stage;
-
-
 
     let { data: stencils, error } = await supabase
       .from("sstatus")
       .select("*, stencils(title, category(cname))")
       .eq("sid", sid)
       .eq("year", year);
-
-      if(week < 3){
-        stencils = stencils.filter((stencil) => stencil.week == week);
-      }     
-
-    if (stencils.length < 1) {
-      router.push({
-        pathname: "/volunteer/enterID",
-        query: { error: "400" },
-      });
-    } else {
-
-      stencils[0].category=stencils[0].stencils.category.cname;
-      stencils[0].title=stencils[0].stencils.title;
-      stencils[0].week=week;
-      stencils[0].year=year;
-      stencils[0].stage=stage;
-      const query = stencils[0];
-      router.push({
-        pathname: "/volunteer/confirm",
-        query: query,
-      });
+    if (week < 3) {
+      stencils = stencils.filter((stencil) => stencil.week == week);
     }
-  };
 
-  if (router.query.error) {
-    message = <text className="text-red-500 text-3xl">Invalid ID</text>;
-  } else {
-    message = <div></div>;
-  }
+    let max = stencils.length;
+    let i = 0;
+
+    while (i < max) {
+      console.log(stencils[i]);
+      if (!stencils[i].tracing_start) {
+        stencils[i].category = stencils[i].stencils.category.cname;
+        stencils[i].title = stencils[i].stencils.title;
+        stencils[i].week = stencils[i].week;
+        stencils[i].year = stencils[i].year;
+        stencils[i].stage = stencils[i].stage;
+        const query = stencils[i];
+        console.log("test");
+        router.push({
+          pathname: "/volunteer/confirm",
+          query: query,
+        });
+        break;
+      }
+      i++;
+    }
+    router.push({
+      pathname: "/volunteer/enterID",
+      query: { error: "400" },
+    });
+  };
+    if (router.query.error) {
+      message = <text className="text-red-500 text-1xl">Sorry, it looks like this stencil has already been completed. Please return this stencil to event staff.</text>;
+    } else {
+      message = <div></div>;
+    }
+
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
@@ -76,23 +70,23 @@ export default function Home() {
 
   return (
     <SignInPrompt>
-    <div className={styles.container}>
-      <h1 className={styles.title}>Enter Stencil ID</h1>
-      <input
-        type="text"
-        id="pid"
-        name="pid"
-        className={styles.input}
-        onKeyDown={handleKeyPress}
-      />
-      {message}
-      <button onClick={getStencil} className={styles.button}>
-        Confirm
-      </button>
-      <Link className={styles.back} href="/volunteer/thankYou">
-        Go Back
-      </Link>
-    </div>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Enter Stencil ID</h1>
+        <input
+          type="text"
+          id="pid"
+          name="pid"
+          className={styles.input}
+          onKeyDown={handleKeyPress}
+        />
+        {message}
+        <button onClick={getStencil} className={styles.button}>
+          Confirm
+        </button>
+        <Link className={styles.back} href="/volunteer/thankYou">
+          Go Back
+        </Link>
+      </div>
     </SignInPrompt>
   );
 }
