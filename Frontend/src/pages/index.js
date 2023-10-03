d6fb8849191f2a7b3f519cf9bf371c95d4c36482
Kyle Goben <kygoben@gmail.com>
2023-10-03 10:22:06 -0500

    volunteer redirect

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,26 +1,14 @@
-import React from "react";
-import Link from "next/link";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-export default function index() {
-  return (
-    <div
-      className="min-h-screen flex flex-col items-center justify-center bg-gray-800 bg-opacity-50 bg-cover"
-      style={{ backgroundImage: 'url("https://www.reimangardens.com/wp-content/gallery/home/Sycamore-Falls-1.jpg")' }}
-    >
-      <div className="space-y-4">
-        <Link
-          href="/volunteer/enterID"
-          className="inline-block bg-white py-2 px-4 rounded-full shadow-md hover:bg-gray-100 transition duration-300"
-        >
-          Click here to go to volunteer page
-        </Link>
-        <Link
-          href="/admin/status"
-          className="inline-block bg-white py-2 px-4 rounded-full shadow-md hover:bg-gray-100 transition duration-300"
-        >
-          Click here to go to admin page
-        </Link>
-      </div>
-    </div>
-  );
+const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/volunteer/enterID');
+  }, [router]);
+
+  return <div>Redirecting...</div>;
 }
+
+export default Home;
