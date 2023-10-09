f84951fe62897c1f363c82af6106d8b33b58e2c8
Kyle Goben <kygoben@gmail.com>
2023-10-09 17:30:13 -0500

    smaller

--- Frontend/src/components/AdminNav.js
+++ Frontend/src/components/AdminNav.js
@@ -14,9 +14,9 @@ const Navbar = ({ total, finished }) => {
 
   return (
     <div className="bg-white p-4 shadow-md">
-      <div className="flex justify-between ">
+      <div className="flex justify-between items-center">
           <div className="text-orange-500 font-bold text-3xl tracking-wider">GourdGuardian</div>
-          <ProgressBar total={total} finished={finished} className="bg-yellow-800 h-2 rounded-full" />
+          <ProgressBar total={total} finished={finished} className="bg-orange-500 h-5 rounded" />
     
         <div className="flex space-x-2">
           <a href="/admin/volunteerControls" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">VolunteerLocks</a>
