509a00c4891f4cbe6e2cfac05c8fe7291911c092
Kyle Goben <kygoben@gmail.com>
2023-11-29 13:20:10 -0600

    navbar naming changed

--- Frontend/src/components/AdminNav.js
+++ Frontend/src/components/AdminNav.js
@@ -37,10 +37,10 @@ const Navbar = ({ total, finished, stage }) => {
           <ProgressBar total={total} finished={finished} className="bg-orange-500 h-5 rounded" />
     
         <div className="flex space-x-2">
-          <a href="/admin/stencil" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Stencil Update</a>
-          <a href="/admin/select" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Manage</a>
+          <a href="/admin/stencil" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Manage Stencils</a>
+          <a href="/admin/select" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Selection</a>
           <a href="/admin/volunteerControls" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">VolunteerLocks</a>
-          <a href="/admin/status" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Status</a>
+          <a href="/admin/status" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Event Management</a>
           <a onClick={handleSignOut} href="/" className="px-3 py-1 rounded-md bg-orange-500 text-white transition-colors hover:bg-brown-700 font-semibold">Logout</a>
         </div>
       </div>
